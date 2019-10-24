4cc2c35290a6c523f8cb7279fa738610b998dbc9
morena-au <mor@mgmt.au.dk>
2019-10-24 17:36:58 +0200

    create functions for scrape 01 and 02

--- scrape.py
+++ scrape.py
@@ -1,25 +1,64 @@
 # Import library
 import requests
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, Comment
 
+# dictionary to collect all websites' content
 data = {}
 
-# Get content from url
-source = requests.get(
-    'https://www.tomsguide.com/us/alexa-vs-siri-vs-google,review-4772.html').text
 
-# Content formatting
-soup = BeautifulSoup(source, 'lxml')
+def scrape01(link='https://www.tomsguide.com/us/alexa-vs-siri-vs-google,review-4772.html'):
+    '''
+    Scrape content from link 1.
+    '''
 
-article = soup.find('article')
+    # Get content from url
+    source = requests.get(link).text
 
-# Put title, summary and body all together
-title = article.h1.text
-summary = article.find('p', class_='strapline').text
-content = [title, summary]
-article_body = article.find('div', class_='text-copy bodyCopy auto')
-for i in article_body.find_all('p'):
-    # append all paragraphs within the article body
-    content.append(i.text)
+    # Content formatting
+    soup = BeautifulSoup(source, 'lxml')
 
-data[0] = content
+    article = soup.find('article')
+
+    # Put title, summary and body all together
+    title = article.h1.text
+    summary = article.find('p', class_='strapline').text
+    content = [title, summary]
+    article_body = article.find('div', class_='text-copy bodyCopy auto')
+    for i in article_body.find_all('p'):
+        # append all paragraphs within the article body
+        content.append(i.text)
+
+    return content
+
+
+def scrape02(link='https://thewirecutter.com/reviews/amazon-echo-vs-google-home/'):
+    '''
+    Scrape content from link 2.
+    '''
+
+    # Get content from url
+    source = requests.get(link)
+    print(source)
+
+    # Content formatting
+    soup = BeautifulSoup(source, 'lxml')
+
+    article = soup.find('article')
+
+    # Put title, summary and body all together
+    title = article.h1.text
+    summary = article.find('section', class_='_1f90b2b1').p.text
+    content = [title, summary]
+
+    section1 = article.find('section', class_='db3dab13')
+    for i in section1.find_all('p'):
+        content.append(i.text)
+
+    section2 = article.find('div', class_='c670ff69')
+    for sec in section2.find_all('section'):
+        for par in sec.find_all('p'):
+            content.append(par.text)
+
+    # Comments TODO
+
+    return content
