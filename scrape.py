8c00c981810c47861301537ba2134371be7d32b0
morena-au <mor@mgmt.au.dk>
2019-10-25 13:54:04 +0200

    scrape 02 link - comments section

--- scrape.py
+++ scrape.py
@@ -1,6 +1,7 @@
 # Import library
 import requests
-from bs4 import BeautifulSoup, Comment
+from bs4 import BeautifulSoup
+import json
 
 # dictionary to collect all websites' content
 data = {}
@@ -31,17 +32,18 @@ def scrape01(link='https://www.tomsguide.com/us/alexa-vs-siri-vs-google,review-4
     return content
 
 
-def scrape02(link='https://thewirecutter.com/reviews/amazon-echo-vs-google-home/'):
+def scrape02(link='https://thewirecutter.com/reviews/amazon-echo-vs-google-home/',
+             comment_link='https://disqus.com/embed/comments/?base=default&f=thewirecutter&t_i=1000%20https%3A%2F%2Fthewirecutter.com%2F%3Fpost_type%3Dreview%26p%3D1000&t_u=https%3A%2F%2Fthewirecutter.com%2Freviews%2Famazon-echo-vs-google-home%2F&t_e=Amazon%20Echo%20vs.%20Google%20Home%3A%20Which%20Voice%20Controlled%20Speaker%20Is%20Best%20for%20You%3F&t_d=Amazon%20Echo%20vs.%20Google%20Home%3A%20Which%20Voice%20Controlled%20Speaker%20Is%20Best%20for%20You%3F&t_t=Amazon%20Echo%20vs.%20Google%20Home%3A%20Which%20Voice%20Controlled%20Speaker%20Is%20Best%20for%20You%3F&s_o=default#version=049b9d4c8356e0d7fe6487ff57f30ea3'):
     '''
     Scrape content from link 2.
     '''
 
     # Get content from url
-    source = requests.get(link)
-    print(source)
+    source = requests.get(link).text
 
     # Content formatting
     soup = BeautifulSoup(source, 'lxml')
+    # print(soup.prettify())
 
     article = soup.find('article')
 
@@ -59,6 +61,33 @@ def scrape02(link='https://thewirecutter.com/reviews/amazon-echo-vs-google-home/
         for par in sec.find_all('p'):
             content.append(par.text)
 
-    # Comments TODO
+    # Comments from external iframe
+    comment_source = requests.get(comment_link).text
+    comment_soup = BeautifulSoup(comment_source, 'lxml')
+
+    post_body = comment_soup.find('script', id='disqus-threadData')
+
+    # Make post body a valid json > extracting the dict-like content inside
+    # <script id="disqus-threadData" type="text/json"> ... </script>
+    # and load it using json library
+    json_dict = json.loads(str(post_body).split(
+        'json">')[1].split('</script')[0])
+
+    # Access the list inside two nested dict
+    posts_list = json_dict['response']['posts']
+
+    for i in posts_list:
+        # Turn message to BeautifulSoup object
+        message = BeautifulSoup(i['message'], 'lxml')
+
+        # Put paragraphs together within the same list's element
+        comment = ''
+
+        # Extract paragraphs within a post
+        for par in message.find_all('p'):
+            comment = comment + ' ' + par.text
+
+        # Append the single comment
+        content.append(comment)
 
     return content
