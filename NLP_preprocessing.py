f150a09c8294cc5e251098d121e985ee5e15c7e8
Morena Rivato <au517585@uni.au.dk>
2020-03-26 09:44:14 +0100

    write preprocessed file + descriptive viz

--- NLP_preprocessing.py
+++ NLP_preprocessing.py
@@ -14,6 +14,7 @@ from nltk.stem import WordNetLemmatizer
 import re
 from sklearn.model_selection import train_test_split
 import html
+import NLP_visualization as NLP_vis
 #import twokenize as ark
 from spellchecker import SpellChecker
 #import codecs
@@ -240,71 +241,20 @@ def clean_comment(comment, lemma=True, del_tags = ['NUM', 'PRON', 'ADV', 'DET', 
 # Apply function to clean the comment
 df['clean_text'] = df.clean_text.apply(clean_comment)
 
-df.to_csv('sub_onetree.csv', index=False, encoding='utf-8')
+#df.to_csv('sub_onetree.csv', index=False, encoding='utf-8')
+# import new granularity file
+#df = pd.read_csv(".\\sub_onetree.csv", encoding='utf8')
 
-# FROM HERE
-
-# remove rows with less than 2 word
-df = df[df['clean_body'].map(lambda x: len(str(x).strip().split())) >= 2]
-
-#df.to_csv('50miclean.csv', index = True)
-# Train Bigram and Trigram Models
-# higher threshold fewer phrases.
-comment_token_list = df['clean_body'].map(lambda x: str(re.sub(r'[_]', ' ',x)).strip().split())
-
-bigram = gensim.models.Phrases(comment_token_list, min_count=200, threshold=25)
-trigram = gensim.models.Phrases(bigram[comment_token_list], min_count=200, threshold=25)
-
-dist = nltk.FreqDist(
-    [word for comment in trigram[bigram[comment_token_list]] for word in comment if '_' in word])
-
-# Sort frequency
-print('Sorted trigrams: \n')
-print(sorted(dist.items(), key=lambda x: x[1], reverse=True))
-print('-'*20)
-
-df['clean_body'] = list(trigram[bigram[comment_token_list]])
-df['clean_body'] = df['clean_body'].map(lambda x: ' '.join(x))
+# drop rows left without comments
+#df.drop(df.index[df.clean_text == "",].tolist(), axis=0, inplace=True)
+df.drop(df.index[df.clean_text.isna(),].tolist(), axis=0, inplace=True)
 
 # write df
-# df.to_csv('trigram_comments.csv', index=True)
+df.to_csv('sub_onetree.csv', index=False, encoding='utf-8')
 
-# function to plot most frequent terms
-def freq_words(x, ascending=False, terms = 30):
-  all_words = ' '.join([text for text in x])
-  all_words = all_words.split()
+# Descriptive visualization
+NLP_vis.freq_words(df["clean_text"], True, 50)
+NLP_vis.words_count(df["clean_text"])
 
-  fdist = nltk.FreqDist(all_words)
-  words_df = pd.DataFrame({'word':list(fdist.keys()), 'count':list(fdist.values())})
 
-  # selecting top most frequent words
-  d = words_df.sort_values("count", ascending=ascending)
-  plt.figure(figsize=(20,5))
-  ax = sns.barplot(data=d[:terms], x= "word", y = "count")
-  ax.set(ylabel = 'Count')
-  plt.xticks(rotation=45)
-  plt.show()
 
-#freq_words(df['clean_body'], False, 50)
-
-# Frequency distribution of word counts in documents
-df = pd.read_csv('preprocessed_comments10k.csv', index_col=0)
-
-words_len = [len(words) for words in df.clean_body]
-
-plt.figure(figsize=(16,7), dpi=160)
-plt.hist(words_len, bins = 500, color='navy')
-plt.text(1000, 450, "Mean   : " + str(round(np.mean(words_len))))
-plt.text(1000, 400, "Median : " + str(round(np.median(words_len))))
-plt.text(1000, 350, "Stdev   : " + str(round(np.std(words_len))))
-plt.text(1000, 300, "1%ile    : " + str(round(np.quantile(words_len, q=0.01))))
-plt.text(1000,  250, "99%ile  : " + str(round(np.quantile(words_len, q=0.99))))
-plt.gca().set(xlim=(0, 4100), ylabel='Number of Documents', xlabel='Document Word Count')
-plt.tick_params(size=16)
-plt.xticks(np.linspace(0,max(words_len),18))
-plt.title('Distribution of Document Word Counts - 10k', fontdict=dict(size=22))
-plt.savefig('WordCount')
-plt.show()
-
-# write the csv file
-df.to_csv('preprocessed_comments100mi.csv', index = True)
